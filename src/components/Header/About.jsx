92cd178093baea9f85b960c440ae97fca10e84ae
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-09-07 19:45:10 +0300

    dd

--- src/components/Header/About.jsx
+++ src/components/Header/About.jsx
@@ -6,7 +6,7 @@ import AllCategorys from '../../AllCategorys.json'
 const About = () => {
   return (
     <>
-    <h8>ggggggg</h8>
+    <h8></h8>
     {AllCategorys.map((item)=>(
     <div key={item.id}>
         {/* <Allabout {...item}/> */}
